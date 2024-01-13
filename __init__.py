a57b3aa24904ec2d48aa7e695271beddcf700f26
blueraincoatli <142435478+blueraincoatli@users.noreply.github.com>
2024-01-13 14:24:51 +0800

    Fix bug that caused import failure

--- __init__.py
+++ __init__.py
@@ -3,22 +3,16 @@ import comfy
 import comfy.utils
 
 from .screenshots import Screenshots
-from .groupBypassCn import BypassGroupControlNode
-
 
 # 设置每个节点的CATEGORY属性确保它们在UI中被归类到"Blueraincoat"组
 Screenshots.CATEGORY = "Blueraincoat"
-BypassGroupControlNode.CATEGORY = "Blueraincoat"
-
 
 NODE_CLASS_MAPPINGS = {
     "Screenshots": Screenshots,
-    "BypassGroupControlNode": BypassGroupControlNode,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "Screenshots": "Screen Shots",
-    "BypassGroupControlNode": "Bypass Group Control Node",
 }
 
 __all__ = [NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS]
